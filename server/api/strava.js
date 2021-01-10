42d68176d8c5d1be32ea96abdf3e669a129b4eab
Johannes <s8jowock@stud.uni-saarland.de>
2021-01-10 14:53:22 +0100

    strava api support added; sports data could now be imported from strava; tested with my strava account; simple preprocessing / statistic extraction done (total active time of all activities over the last seven days)

---
+++ server/api/strava.js
@@ -0,0 +1,126 @@
+
+const https = require('https')
+const fs = require('fs')
+
+class StravaSportsDataCollector{
+    constructor() {
+
+    }
+
+
+    loadTestData(callback) {
+        return new Promise((resolve, reject) => {
+            fs.readFile('strava_data.json', 'utf8' , (err, data) => {
+                if (err) {
+                    // todo error logger
+                    console.error(err)
+                    reject(err)
+                    return
+                }
+                resolve(callback(data))
+            })
+        })
+    }
+
+    static extractActivityCoreElements(activity) {
+        var name = activity.name
+        var startDate = activity.start_date
+        var movingTime = activity.moving_time / (60*60)
+        var id = activity.id
+
+        return [id, name, startDate, movingTime]
+    }
+
+    static activityTimeFilter(activity) {
+        var activityDate = Date.parse(activity.start_date)
+        var today = new Date();
+        // timeDistance in hours (original difference value: miliseconds)
+        var timeDistance = (today - activityDate) / (60*60*1000)
+
+        // todo: put compare value / sliding window into config
+        return timeDistance < (24*14)
+    }
+
+    handleSportsData(activityData) {
+        //console.log(activityData)
+        var selectedActvities = []
+        var parsedActivityData = JSON.parse(activityData)
+        var filteredActivities = parsedActivityData.filter(StravaSportsDataCollector.activityTimeFilter)
+        for (let activity of filteredActivities) {
+            var data = StravaSportsDataCollector.extractActivityCoreElements(activity)
+            selectedActvities.push(data)
+        }
+
+        console.log("Activities in considered time interval: " + filteredActivities.length)
+        return selectedActvities
+    }
+
+    async queryActivityData(athleteAccessToken, callback) {
+        return new Promise((resolve, reject) => {
+            const options = {
+                hostname: 'www.strava.com',
+                port: 443,
+                path: '/api/v3/athlete/activities?access_token=' + athleteAccessToken,
+                method: 'GET'
+            }
+
+            // server response
+            var response = ""
+            const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+
+                res.on('data', d => {
+                    // gather sports response data (activity data is too large for one single packet, therefore we need to sample it back from the packets)
+                    // console.log("-- data event --\n")
+                    response += d
+                })
+
+                res.on('end', d => {
+                    // console.log("-- end event --\n")
+                    // after response transmission completed, process it
+                    if (res.statusCode == 200) {
+                        resolve(callback(response))
+                    }
+                })
+            })
+
+            req.on('error', error => {
+                // TODO: use logger
+                console.error(error)
+                reject(error)
+            })
+
+            req.end()
+        })
+    }
+
+
+    getStastics(activities) {
+        var sumSportsHours = 0
+        for (var activity of activities) {
+            sumSportsHours += activity[3]
+        }
+        return [activities.length, sumSportsHours]
+    }
+
+    async getSportsStatistics(athleteAccessToken) {
+        // todo: put access code into config
+        var selectedActvities = await this.queryActivityData(athleteAccessToken, this.handleSportsData)
+        // selectedActvities = await loadTestData(handleSportsData)
+        // console.log(selectedActvities)
+        var activityStatistics = this.getStastics(selectedActvities)
+        return activityStatistics
+    }
+
+    testCall() {
+        var athlete1Token = "26c6f5b33f7c8ac150c6ac4b4c154c845fa096ad"
+        console.log("stats:")
+        this.getSportsStatistics(athlete1Token)
+            .then((stats) => {console.log(stats)})
+    }
+}
+
+
+stravaCon = new StravaSportsDataCollector()
+
+stravaCon.testCall()
