d81c2482dd89ceac54e236ac9376bacbadfa55bc
xaver.fabian <s8xafabi@stud.uni-saarland.de>
2021-01-13 15:50:23 +0100

    A simple socket-io client that can connect to the server. you can use the repl to send messages to the server. Listens for requestID events and then sends id and bool if they are ball or motor controller

--- server/socketClient.js
+++ server/socketClient.js
@@ -6,9 +6,10 @@ socket.on('connect', () => {
     console.log("Successfuly conneczted")
 });
 
-socket.on('message', function (data) {
+/*socket.on('message', function (data) {
   console.log(data);
 });
+*/
 
 socket.on('go', function () {
     console.log("Received go Command !");
@@ -16,5 +17,19 @@ socket.on('go', function () {
 
 socket.on("requestID", () => {
     console.log("Request received Registering....")
-    socket.emit("registerID", "2", true); // your id you need to create a client and then switch the code here to false to create a controller client 
+    socket.emit("registerID", "1", false); // your id you need to create a client and then switch the code here to false to create a controller client 
+})
+// Create 3 clients and let them live even if you kill the server. Websockets are smart enough to reconnect again
+// See https://stackoverflow.com/questions/15509231/unit-testing-node-js-and-websockets-socket-io for unit tests
+
+const repl = require('repl');
+const chalk = require('chalk');
+repl.start({
+  prompt: '',
+  eval: (cmd) => {
+    let event, args;
+      [event, args] = cmd.split(" ") // the args do not have the correct type since they ayre strings so be careful !
+      console.log(event);
+      socket.emit(event, args)
+  }
 })
