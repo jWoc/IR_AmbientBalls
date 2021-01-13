4a5c9d45d60726e7e0591b17b0e81cf3438d8468
BrindhaJeniefer-Joseph <rexjeniefer@gmail.com>
2021-01-13 23:11:09 +0100

    adding client_test file

---
+++ client/client_test.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#define PORT 8080 
+   
+int main(int argc, char const *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    //valread = read( sock , buffer, 1024); 
+    //printf("%s\n",buffer ); 
+    return 0; 
+}
+
+
+// got a very nice reference from below link
+/// https://stackoverflow.com/questions/64911095/example-of-c-client-and-node-js-server?newreg=18bda68644d94641bc20e2deab47998a
