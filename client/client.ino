3e4b6df4a5389ae540808bf7870dba716fe3da6c
BrindhaJeniefer-Joseph <67753756+BrindhaJeniefer-Joseph@users.noreply.github.com>
2021-02-27 17:45:30 +0100

    Update client.ino

--- client/client.ino
+++ client/client.ino
@@ -1,66 +1,49 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
 
-const char* ssid = "wifiNetworkName";
-const char* password = "wifiNetworkPassword"; 
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoJson.h>
+
+#include <WebSocketsClient.h>
+#include <SocketIoClient.h>
+
+#include <Hash.h>
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIoClient socketIO;
+
+void event(const char * payload, size_t length) {
+  USE_SERIAL.printf("got message: %s\n", payload);
+}
 
 void setup() {
+    USE_SERIAL.begin(115200);
 
-    Serial.begin(115200);
+    USE_SERIAL.setDebugOutput(true);
 
-    Serial.println();
-    Serial.println();
-    Serial.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
-        Serial.printf("[SETUP] WAIT %d...\n", t);
-        Serial.flush();
-        delay(1000);
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("Internet-QI-119", "QI.W-LAN!neu*23072019");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
     }
 
-    WiFi.begin("Galaxy", "12345678");  //WiFi.begin("ssid", "password")
-    while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    }
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-
+    socketIO.on("event", event);
+    socketIO.begin("192.168.178.111", 3000);
+    // use HTTP Basic Authorization this is optional remove if not needed
+    //webSocket.setAuthorization("username", "password");
 }
 
 void loop() {
-
-    if(WiFi.status() == WL_CONNECTED)
-    {
-        HTTPClient http;
-        Serial.print("[HTTP] begin...\n");
-      
-        // configure server and url
-        http.begin("http://localhost:8080/");  // desired url, instead of localhost -> give the ip of the system where the server is running 
-      
-        Serial.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                Serial.println(payload);
-            }
-        } 
-           else {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
-    }
-    delay(5000);
+    socketIO.loop();
 }
